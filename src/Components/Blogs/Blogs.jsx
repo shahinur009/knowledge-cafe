11ad6d1868141c83214cc2bdba0652e80b484897
shahin009 <shaheenmis170879@gmail.com>
2024-03-13 00:00:02 +0600

    first commit

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(()=>{
+        fetch('BlogsData.json')
+        .then(res => res.json())
+        .then(data=>setBlogs(data))
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
